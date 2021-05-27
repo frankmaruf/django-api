db71164a849b80f78609f6bf41944aad27a5e4c9
Abdullah Al Maruf <thefrankmaruf@gmail.com>
2021-05-27 12:38:56 +0600

    simple api view set

--- DjangoApi/api_basic/views.py
+++ DjangoApi/api_basic/views.py
@@ -1,6 +1,44 @@
 from django.shortcuts import render
-from rest_framework.renderers import JSONRenderer
+from rest_framework import status
 from rest_framework.parsers import JSONParser
+from rest_framework.response import Response
 from .models import Article
-from .serializers import ArticleModelSerializer
+from .serializers import ArticleModelSerializer,ArticleSerializer
+from rest_framework.decorators import api_view
 # Create your views here.
+
+
+@api_view(['GET', 'POST'])
+def article_list(request):
+
+    if request.method == 'GET':
+        articles = Article.objects.all()
+        serializer = ArticleModelSerializer(articles, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = ArticleModelSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET', 'PUT','DELETE'])
+def article_detail(request, pk):
+    try:
+        article = Article.objects.get(pk=pk)
+    except Article.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    if request.method == "GET":
+        serializer = ArticleModelSerializer(article, many=False)
+        return Response(serializer.data)
+    elif request.method == "PUT":
+        serializer = ArticleModelSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == "DELETE":
+        article.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
